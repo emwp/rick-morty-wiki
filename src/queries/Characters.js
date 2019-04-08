d06c749b07c54bf565e0fb2c8c89dd6b5d61e845
Everton Pereira <everton.mwp@gmail.com>
2019-04-07 21:54:57 -0300

    Characters.js graphQL query

---
+++ src/queries/Characters.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { View, Text, FlatList } from 'react-native';
+import { graphql, QueryRenderer } from 'react-relay';
+import Card from '../components/Card';
+
+import environment from '../environment/Environment';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <QueryRenderer
+        environment={environment}
+        query={graphql`
+          query CharactersQuery {
+            characters(page: 1) {
+              results {
+                id
+                name
+                status
+                species
+                image
+              }
+            }
+          }
+        `}
+        variables={{ page: 1 }}
+        render={({ error, props }) => {
+          if (error) {
+            return (
+              <View>
+                <Text>Error!</Text>
+              </View>
+            );
+          }
+          if (!props) {
+            return (
+              <View>
+                <Text>Loading!</Text>
+              </View>
+            );
+          }
+          return (
+            <FlatList
+              data={props.characters.results}
+              keyExtractor={item => item.id.toString()}
+              renderItem={({ item }) => (
+                <Card
+                  id={item.id.toString()}
+                  name={item.name}
+                  status={item.status}
+                  species={item.species}
+                  image={item.image}
+                />
+              )}
+            />
+          );
+        }}
+      />
+    );
+  }
+}
---
+++ src/queries/__generated__/CharactersQuery.graphql.js
@@ -0,0 +1,139 @@
+/**
+ * @flow
+ * @relayHash 2cc67f1f26e0193832bf90463d3ce5e0
+ */
+
+/* eslint-disable */
+
+'use strict';
+
+/*::
+import type { ConcreteRequest } from 'relay-runtime';
+export type CharactersQueryVariables = {||};
+export type CharactersQueryResponse = {|
+  +characters: ?{|
+    +results: ?$ReadOnlyArray<?{|
+      +id: ?string,
+      +name: ?string,
+      +status: ?string,
+      +species: ?string,
+      +image: ?string,
+    |}>
+  |}
+|};
+export type CharactersQuery = {|
+  variables: CharactersQueryVariables,
+  response: CharactersQueryResponse,
+|};
+*/
+
+
+/*
+query CharactersQuery {
+  characters(page: 1) {
+    results {
+      id
+      name
+      status
+      species
+      image
+    }
+  }
+}
+*/
+
+const node/*: ConcreteRequest*/ = (function(){
+var v0 = [
+  {
+    "kind": "LinkedField",
+    "alias": null,
+    "name": "characters",
+    "storageKey": "characters(page:1)",
+    "args": [
+      {
+        "kind": "Literal",
+        "name": "page",
+        "value": 1,
+        "type": "Int"
+      }
+    ],
+    "concreteType": "Characters",
+    "plural": false,
+    "selections": [
+      {
+        "kind": "LinkedField",
+        "alias": null,
+        "name": "results",
+        "storageKey": null,
+        "args": null,
+        "concreteType": "Character",
+        "plural": true,
+        "selections": [
+          {
+            "kind": "ScalarField",
+            "alias": null,
+            "name": "id",
+            "args": null,
+            "storageKey": null
+          },
+          {
+            "kind": "ScalarField",
+            "alias": null,
+            "name": "name",
+            "args": null,
+            "storageKey": null
+          },
+          {
+            "kind": "ScalarField",
+            "alias": null,
+            "name": "status",
+            "args": null,
+            "storageKey": null
+          },
+          {
+            "kind": "ScalarField",
+            "alias": null,
+            "name": "species",
+            "args": null,
+            "storageKey": null
+          },
+          {
+            "kind": "ScalarField",
+            "alias": null,
+            "name": "image",
+            "args": null,
+            "storageKey": null
+          }
+        ]
+      }
+    ]
+  }
+];
+return {
+  "kind": "Request",
+  "fragment": {
+    "kind": "Fragment",
+    "name": "CharactersQuery",
+    "type": "Query",
+    "metadata": null,
+    "argumentDefinitions": [],
+    "selections": (v0/*: any*/)
+  },
+  "operation": {
+    "kind": "Operation",
+    "name": "CharactersQuery",
+    "argumentDefinitions": [],
+    "selections": (v0/*: any*/)
+  },
+  "params": {
+    "operationKind": "query",
+    "name": "CharactersQuery",
+    "id": null,
+    "text": "query CharactersQuery {\n  characters(page: 1) {\n    results {\n      id\n      name\n      status\n      species\n      image\n    }\n  }\n}\n",
+    "metadata": {}
+  }
+};
+})();
+// prettier-ignore
+(node/*: any*/).hash = 'ee5f007eb7491954c192bd5519b157dc';
+module.exports = node;
